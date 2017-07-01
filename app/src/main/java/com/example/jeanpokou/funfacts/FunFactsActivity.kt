6577bb5dfbb366c05f8fa014ce0ffce1a16d45d7
jeanpokou <pokoujeanmarc@gmail.com>
2017-07-01 18:47:50 -0300

    Adding Anko support

--- app/src/main/java/com/example/jeanpokou/funfacts/FunFactsActivity.kt
+++ app/src/main/java/com/example/jeanpokou/funfacts/FunFactsActivity.kt
@@ -1,16 +1,13 @@
 package com.example.jeanpokou.funfacts
 
-import android.graphics.Color
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_fun_facts.*
-import java.util.*
 
 class FunFactsActivity : AppCompatActivity() {
-    //val TAG = FunFactsActivity.class.getSimpleName();
     val factBook = FactBook()
     val colorWheel = ColorWheel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fun_facts)
